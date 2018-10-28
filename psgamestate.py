fd4ac8252258dd3196ca46e4ff1ff638215179ac
cminsung <38871509+cminsung@users.noreply.github.com>
2018-10-28 14:47:10 -0700

    Add files via upload

--- psgamestate.py
+++ psgamestate.py
@@ -3,3 +3,29 @@ Created on Oct 28, 2018
 
 @author: cmins
 '''
+
+class PSOutOfBoundsException(Exception):
+    pass
+
+class PSInvalidMove(Exception):
+    pass
+
+class PSGameOverError(Exception):
+    pass
+
+class PSGameState:
+    def __init__(self, rows:int, cols:int):
+        self.board = psboard()
+
+
+def updateBoard():
+    pass
+
+def isFinished():
+    pass
+
+def isValidMove(fromrow:int, fromcol:int, torow:int, tocol:int) -> bool:
+    pass
+
+def isOutOfBounds():
+    pass
