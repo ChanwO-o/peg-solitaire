959b35559720fe4d10a5b1ac65801d1fbd2fa8ac
Chan Woo Park <14cpark@gmail.com>
2018-11-27 19:55:51 -0800

    add getboard function, implement some board functions

--- psgamestate.py
+++ psgamestate.py
@@ -18,23 +18,28 @@ class PSGameState:
     
     def __init__(self, board:[[int]]):
 ##        self.board = createNewBoard(rows, cols)
-        pass
+        self.board = board
 
     def get(self, row:int, col:int) -> int:
         ''' Returns value of peg at coordinate (-1 0 or 1) '''
-        pass
+        # TODO check if coordinate is not outofbounds
+        return self.board[row][col]
 
     def set(self, row:int, col:int, val:int) -> None:
         ''' Sets coordinate value to val '''
-        pass
+        self.board[row][col] = val
+
+    def getBoard(self) -> [[int]]:
+        ''' Returns the board '''
+        return self.board
 
     def getRows(self) -> int:
         ''' Returns number of rows of board '''
-        pass
+        return len(self.board)
 
     def getCols(self) -> int:
         ''' Returns number of cols of board '''
-        pass
+        return len(self.board[0])
         
     def updateBoard():
         pass
