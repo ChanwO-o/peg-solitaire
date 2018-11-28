29f63bd600c7b9d840bde150c5e1db48704b98b1
Chan Woo Park <14cpark@gmail.com>
2018-11-27 18:58:49 -0800

    update isOutOfBounds function

--- psgamestate.py
+++ psgamestate.py
@@ -31,5 +31,10 @@ class PSGameState:
     def isValidMove(fromrow:int, fromcol:int, torow:int, tocol:int) -> bool:
         pass
 
-def isOutOfBounds():
-    pass
+    def isOutOfBounds(row:int, col:int) -> bool:
+        ''' Checks if location is in board '''
+        if row < 0 or row > self.getRows():
+            return True
+        if col < 0 or col > self.getCols():
+            return True
+        ## TODO: check for corners
